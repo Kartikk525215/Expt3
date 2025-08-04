268f6c1f765382af334f22e9f78884796a0f16eb
Kartikk525215 <kartik525215@gmail.com>
2025-08-04 16:41:26 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,1 +1,7 @@
-This is additon code in java.
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
