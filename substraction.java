7bec1b81dcf94ebc0b405803efe06787ff2a39f9
BHUSHANMALI2004 <bhushan802047@gmail.com>
2025-08-04 16:42:35 +0530

    Update substraction.java

---
+++ substraction.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 12, b = 2;
+        int sub = a - b;
+        System.out.println("substraction = " + sub);
+    }
+}
